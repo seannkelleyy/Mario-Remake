dd5871975e8a73e591598fcc110177f78e878fba
Sean Kelley <seannkelleyy1@gmail.com>
2024-02-06 13:47:56 -0500

    Fix: Removes Attack method for Enemy and changes walkleft and walkright to change direction.

--- Interfaces/Entities/IEnemy.cs
+++ Interfaces/Entities/IEnemy.cs
@@ -6,17 +6,10 @@ namespace GreenGame.Interfaces
 	public interface IEnemy : ISprite
     {
         // Movement
-        public void WalkLeft();
-
-        public void WalkRight();
+        public void ChangeDirection();
 
         // Function to handle when Enemu takes damage
         void TakeDamage();
-
-        // Actions
-        // This will be a generic attack command. We could get what kind of attack
-        // Based upon the state of the Enemy.
-        public void Attack(Game game);
     }
 }
 
