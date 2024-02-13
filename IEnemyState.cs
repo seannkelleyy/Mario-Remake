3b4c5a1545e5a24c17c58d2e99169d3e72e59b53
Dominic Artman <dominicartman@gmail.com>
2024-02-12 22:54:26 -0500

    Created IEnemyState

---
+++ IEnemyState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mario.Interfaces;
+using Microsoft.Xna.Framework;
+
+namespace GreenGame.Interfaces
+{
+	public interface IEnemyState : IEnemy
+	{
+        void ChangeDirection();
+        void BeStomped();
+        void BeFlipped();
+        void Update();
+    }
+}
