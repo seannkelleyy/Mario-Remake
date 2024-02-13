5879f482f738bc0031581db802e536047b63d72f
Sean Kelley <seannkelleyy1@gmail.com>
2024-02-12 20:41:08 -0500

    Refactor: Removes DisplaySpriteCommand and SpriteState. Adds RelayCommand, which uses deletegates to allow us to create commands on the fly.

--- Input/KeyboardController.cs
+++ Input/KeyboardController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
-using Microsoft.Xna.Framework.Content;
 using Mario.Interfaces;
+using GreenGame.Interfaces;
+using Microsoft.Xna.Framework.Content;
+using System;
 
 namespace Mario.Input
 {
@@ -10,35 +12,48 @@ namespace Mario.Input
     {
         private Dictionary<Keys, ICommand> Commands;
 
-        public KeyboardController() {
+        // We will probably just instantiate these here for Spint 2 purposes,
+        // but they will be gone after that.
+        private IItem ItemSprite;
+        private IBlock BlockSprite;
+        // private IEnemy EnemySprite;
+
+
+        public KeyboardController()
+        {
             Commands = new Dictionary<Keys, ICommand>();
         }
 
-        public void LoadCommands(MarioRemake game, ContentManager Content, SpriteBatch spriteBatch) {
-
-
+        // NOTE: When we start saving the state for the character, we will pass in the GameContentManager
+        // to assign the functions to call when keys are pressed.
+        public void LoadCommands(MarioRemake game, ContentManager Content, SpriteBatch spriteBatch)
+        {
             Commands.Add(Keys.Q, new QuitCommand(game));
             Commands.Add(Keys.R, new RestartCommand(game));
 
-            //Jump
-            // Commands.Add(Keys.W, );
-            // Move left
-            // Commands.Add(Keys.A, );
-            // Crouch
-            // Commands.Add(Keys.S, );
-            // Move right
-            // Commands.Add(Keys.D, );
-            // Attack
-            // Commands.Add(Keys.E, );
+            //Commands.Add(Keys.W, );
+            //Commands.Add(Keys.A, );
+            //Commands.Add(Keys.S, );
+            //Commands.Add(Keys.D, );
+            //Commands.Add(Keys.E, );
 
+            Commands.Add(Keys.I, new RelayCommand(new Action(ItemSprite.CycleItemNext)));
+            Commands.Add(Keys.U, new RelayCommand(new Action(ItemSprite.CycleItemPrev)));
+            Commands.Add(Keys.T, new RelayCommand(new Action(BlockSprite.CycleBlockNext)));
+            Commands.Add(Keys.Y, new RelayCommand(new Action(BlockSprite.CycleBlockPrev)));
+
+            // These will be added in enemy ticket.
+            // Commands.Add(Keys.O, new RelayCommand(new Action(EnemySprite.CycleEnemyNext)));
+            // Commands.Add(Keys.P, new RelayCommand(new Action(EnemySprite.CycleEnemyPrev)));
         }
 
         public void Add(Keys key, ICommand command)
         {
-            Commands.Add(key, command); 
+            Commands.Add(key, command);
         }
 
-        public void Update() {
+        public void Update()
+        {
             KeyboardState state = Keyboard.GetState();
             foreach (Keys key in Commands.Keys)
             {
