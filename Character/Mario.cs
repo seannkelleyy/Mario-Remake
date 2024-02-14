5048265a45d8a9dd4d9a9bc05f340bed1a4efe37
Jinyi <103870101+SharkKnightGeis@users.noreply.github.com>
2024-02-13 19:11:50 -0500

    Add Standing and Moving states

--- Character/Mario.cs
+++ Character/Mario.cs
@@ -8,40 +8,29 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 using GreenGame.Interfaces;
+using GreenGame.Interfaces.Entities;
 
 namespace Mario.Character
 {
     public class Mario : IHero
     {
-        private SpriteStates spriteStates;
-        private Dictionary<string, Texture2D> sprites;
-        private Vector2 position;
-        private Texture2D currentSprite;
-        private KeyboardController keyboardController;
+        public IHeroState state;
+        private bool direction;
+        //private Dictionary<string, Texture2D> sprites;
+        //private Vector2 position;
+        //private Texture2D currentSprite;
+        //private KeyboardController keyboardController;
 
         public Mario(Dictionary<string, Texture2D> sprites, KeyboardController keyboardController)
         {
-            this.sprites = sprites;
-            this.keyboardController = keyboardController;
-            InitializeCommands();
-            position = Vector2.Zero; // Starting position, adjust as needed
-            currentSprite = sprites["Standing"]; // Default sprite
-            spriteStates = SpriteStates.Standing; // Default state
-        }
-
-        private void InitializeCommands()
-        {
-            // Example of setting up commands, assuming these are implemented
-            keyboardController.KeyCommands[Keys.Left] = new MoveLeftCommand(this);
-            keyboardController.KeyCommands[Keys.Right] = new MoveRightCommand(this);
-            // Add more command initializations here
+            state = new MarioStandingState(this, direction); // Default state
         }
 
         public void Update(GameTime gameTime)
         {
             // Update Mario's state based on the current command executed
             // This could involve changing spriteStates and currentSprite based on the action
-            keyboardController.Update(); // Process input and execute corresponding commands
+            // keyboardController.Update(); // Process input and execute corresponding commands
 
             // Additional game logic here (e.g., physics, collisions)
         }
@@ -55,28 +44,28 @@ namespace Mario.Character
         public void WalkLeft()
         {
             position.X -= 2; // Move left
-            spriteStates = SpriteStates.WalkingLeft;
+            state = SpriteStates.WalkingLeft;
             currentSprite = sprites["WalkingLeft"];
         }
 
         public void WalkRight()
         {
             position.X += 2; // Move right
-            spriteStates = SpriteStates.WalkingRight;
+            state = SpriteStates.WalkingRight;
             currentSprite = sprites["WalkingRight"];
         }
 
         public void Jump()
         {
             // Jump logic here
-            spriteStates = SpriteStates.Jumping;
+            state = SpriteStates.Jumping;
             currentSprite = sprites["Jumping"];
         }
 
         public void Crouch()
         {
             // Crouch logic here
-            spriteStates = SpriteStates.Crouching;
+            state = SpriteStates.Crouching;
             currentSprite = sprites["Crouching"];
         }
 
---
+++ Interfaces/Entities/IHeroState.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Mario.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenGame.Interfaces.Entities
+{
+    public interface IHeroState
+    {
+        // Movement
+        public void ChangeDirection();
+
+        void Update(GameTime gameTime);
+    }
+}
+
