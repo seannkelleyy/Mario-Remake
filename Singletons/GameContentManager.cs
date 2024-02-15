1a6e7ed7a80d34a0a8a761dca64c81a4baacf512
Sean Kelley <seannkelleyy1@gmail.com>
2024-02-14 22:42:59 -0500

    Feat: Starts adding game content manager

---
+++ Singletons/GameContentManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mario.Singletons
+{
+	public class GameContentManager
+	{
+		private MarioRemake _game;
+
+		public GameContentManager(MarioRemake game)
+		{
+			_game = game;
+		}
+
+		public void Load()
+		{
+
+		}
+
+		public void Update(MarioRemake game, GameTime gameTime)
+		{
+
+		}
+
+		public void Draw(SpriteBatch spriteBatch, MarioRemake Game)
+		{
+
+		}
+	}
+}
+
