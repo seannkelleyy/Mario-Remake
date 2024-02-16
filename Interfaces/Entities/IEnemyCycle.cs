75f1bc0e631cdcc2e89e7c2bf45cab7d4d05ffea
hood276 <hood.276@buckeyemail.osu.edu>
2024-02-16 18:31:36 -0500

    Added IEnemyCycle interface and EnemyCycle class. Also IBlock's inheritance of ISprite because it's not necessary.

---
+++ Interfaces/Entities/IEnemyCycle.cs
@@ -0,0 +1,12 @@
+﻿namespace Mario.Interfaces
+{
+    // This interface is just here for Sprint2, it will be deleted after
+    public interface IEnemyCycle
+    {
+        public void CycleEnemyNext();
+
+        public void CycleEnemyPrev();
+
+    }
+}
+
