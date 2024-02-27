4dcf251a6b41c6a10af40fa37d86d63e37a135e2
Jinyi <103870101+SharkKnightGeis@users.noreply.github.com>
2024-02-27 00:32:37 -0500

    Create ObjectFactory.cs
    I'm assuming that Sprites will be produced by the SpriteFactory class, so the ObjectFactory will not return Sprites. Also, the item Object is the only kind that takes an ISprite[] array, not sure if we should have it here

---
+++ Singletons/ObjectFactory.cs
@@ -0,0 +1,48 @@
+﻿using Mario.Interfaces;
+using Mario.Interfaces.Entities;
+using Mario.Sprites;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Mario.Entities.Character;
+
+namespace Mario.Singletons
+{
+    public class GameObjectFactory
+    {
+        private static GameObjectFactory instance = new GameObjectFactory();
+        private Dictionary<string, Vector2> entityPosition;
+        private ISprite[] items;
+
+        // Singleton instance property
+        public static GameObjectFactory Instance => instance;
+
+        // Private constructor to prevent instantiation
+        private GameObjectFactory() { }
+
+        // Factory method to create game entities
+        public IEntityBase CreateEntity(string type)
+        {
+            switch (type)
+            {
+                case "Mario":
+                    // Assuming Mario implements IHero
+                    return new Hero(entityPosition[type]);
+                case "Goomba":
+                    // Assuming Koopa implements IEnemy
+                    return new Goomba(entityPosition[type]);
+                case "Koopa":
+                    // Assuming Koopa implements IEnemy
+                    return new Koopa(entityPosition[type]);
+                case "Block":
+                    // Assuming Block implements IBlock
+                    return new Block(entityPosition[type]);
+                case "Item":
+                    // Assuming Item implements IItem
+                    return new Item(items, entityPosition[type]);
+                // Add cases for other entities as needed
+                default:
+                    throw new KeyNotFoundException($"Entity type {type} not recognized.");
+            }
+        }
+    }
+}
