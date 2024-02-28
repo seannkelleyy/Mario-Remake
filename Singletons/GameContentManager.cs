8e0be94ad5b44672382bbeffbe274441ed6997b3
Sean Kelley <sean.kelley@neudesic.com>
2024-02-28 11:20:29 -0500

    Feat: Adds add and remove method

--- Singletons/GameContentManager.cs
+++ Singletons/GameContentManager.cs
@@ -1,17 +1,19 @@
 ﻿using Mario.Entities.Character;
+using Mario.Interfaces;
 using Mario.Interfaces.Entities;
-using Mario.Sprites;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System.Collections;
+using System.Linq;
 
 namespace Mario.Singletons
 {
     public class GameContentManager
     {
-        private SpriteFactory spriteFactory;
+        private IHero mario;
+        private IEnemy[] enemies;
+        private IItem[] items;
+        private IBlock[] blocks;
         private static GameContentManager instance = new GameContentManager();
-        private ArrayList entities;
 
         // This code follows the singleton pattern
         // When you need a GCM, you call GameContentManager.Instance
@@ -26,26 +28,77 @@ namespace Mario.Singletons
         // This is a private constructor, so no one can create a new GameContentManager
         private GameContentManager() { }
 
-        public void Initialize()
-        {
-            spriteFactory = SpriteFactory.Instance;
-            entities = new ArrayList();
-        }
-
         public void Load()
         {
             // Will call level loader 
-            entities.Add(new Hero(new Vector2(300, 100)));
+            mario = new Hero(new Vector2(300, 100));
         }
-        public void add(IEntityBase entity) { 
-            entities.Add(entity);
-        }
-        public void remove(IEntityBase entity) { 
-            entities.Remove(entity);
-        }
-        public ArrayList GetEntities()
+
+        public IEntityBase[] GetEntities()
         {
+            IEntityBase[] entities = new IEntityBase[1];
+            entities[0] = mario;
             return entities;
+        }
+
+        public void AddEntity(IEntityBase entity)
+        {
+            if (entity is IHero)
+            {
+                mario = (IHero)entity;
+            }
+            else if (entity is IEnemy)
+            {
+                enemies.Append((IEnemy)entity);
+            }
+            else if (entity is IItem)
+            {
+                items.Append((IItem)entity);
+            }
+            else if (entity is IBlock)
+            {
+                blocks.Append((IBlock)entity);
+            }
+        }
+
+        // Not sure how to procede here, since we don't have a way of 
+        // knowing which entity to remove. Maybe we should add an ID to 
+        // each entity?
+        public void RemoveEntity(IEntityBase entity)
+        {
+            if (entity is IHero)
+            {
+                mario = null;
+            }
+            else if (entity is IEnemy)
+            {
+                // Remove enemy from list
+            }
+            else if (entity is IItem)
+            {
+                // Remove item from list
+            }
+            else if (entity is IBlock)
+            {
+                // Remove block from list
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            mario.Draw(spriteBatch);
+            foreach (IEnemy enemy in enemies)
+            {
+                enemy.Draw(spriteBatch);
+            }
+            foreach (IItem item in items)
+            {
+                item.Draw(spriteBatch);
+            }
+            foreach (IBlock block in blocks)
+            {
+                block.Draw(spriteBatch);
+            }
         }
     }
 }
