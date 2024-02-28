d78a4e81b4acecd10c1927ed811dd765531631f2
Jinyi <103870101+SharkKnightGeis@users.noreply.github.com>
2024-02-28 11:14:44 -0500

    Create ObjectFactory.cs (#57)
    * Create ObjectFactory.cs
    
    I'm assuming that Sprites will be produced by the SpriteFactory class, so the ObjectFactory will not return Sprites. Also, the item Object is the only kind that takes an ISprite[] array, not sure if we should have it here
    
    * Update ObjectFactory.cs
    
    Add Vector2 position parameter so the factory will take it to create the object rather than reading it from a dictionary
    
    * Update Singletons/ObjectFactory.cs
    
    * Update Singletons/ObjectFactory.cs
    
    ---------
    
    Co-authored-by: Sean Kelley <94130791+seannkelleyy@users.noreply.github.com>

---
+++ Singletons/ObjectFactory.cs
@@ -0,0 +1,47 @@
+﻿using Mario.Interfaces;
+using Mario.Interfaces.Entities;
+using Mario.Sprites;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Mario.Entities.Character;
+
+namespace Mario.Singletons
+{
+    public class GameObjectFactory
+    {
+        private static GameObjectFactory instance = new GameObjectFactory();
+        //private Dictionary<string, Vector2> entityPosition;
+
+        // Singleton instance property
+        public static GameObjectFactory Instance => instance;
+
+        // Private constructor to prevent instantiation
+        private GameObjectFactory() { }
+
+        // Factory method to create game entities
+        public IEntityBase CreateEntity(string type, Vector2 position)
+        {
+            switch (type)
+            {
+                case "Mario":
+                    // Assuming Mario implements IHero
+                    return new Hero(position);
+                case "Goomba":
+                    // Assuming Koopa implements IEnemy
+                    return new Goomba(position);
+                case "Koopa":
+                    // Assuming Koopa implements IEnemy
+                    return new Koopa(position);
+                case "Block":
+                    // Assuming Block implements IBlock
+                    return new Block(position);
+                case "Item":
+                    // Assuming Item implements IItem
+                    return new Item(position);
+                // Add cases for other entities as needed
+                default:
+                    throw new KeyNotFoundException($"Entity type {type} not recognized.");
+            }
+        }
+    }
+}
