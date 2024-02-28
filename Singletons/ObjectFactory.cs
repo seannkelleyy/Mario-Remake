ffdfdd887b9cfd4d03922952f0ce2f1c5b0af3d5
FleePompeii <103870101+SharkKnightGeis@users.noreply.github.com>
2024-02-28 11:06:10 -0500

    Update ObjectFactory.cs
    Add Vector2 position parameter so the factory will take it to create the object rather than reading it from a dictionary

--- Singletons/ObjectFactory.cs
+++ Singletons/ObjectFactory.cs
@@ -10,8 +10,8 @@ namespace Mario.Singletons
     public class GameObjectFactory
     {
         private static GameObjectFactory instance = new GameObjectFactory();
-        private Dictionary<string, Vector2> entityPosition;
-        private ISprite[] items;
+        //private Dictionary<string, Vector2> entityPosition;
+        private ISprite[] items;//To be deleted
 
         // Singleton instance property
         public static GameObjectFactory Instance => instance;
@@ -20,25 +20,25 @@ namespace Mario.Singletons
         private GameObjectFactory() { }
 
         // Factory method to create game entities
-        public IEntityBase CreateEntity(string type)
+        public IEntityBase CreateEntity(string type, Vector2 position)
         {
             switch (type)
             {
                 case "Mario":
                     // Assuming Mario implements IHero
-                    return new Hero(entityPosition[type]);
+                    return new Hero(position);
                 case "Goomba":
                     // Assuming Koopa implements IEnemy
-                    return new Goomba(entityPosition[type]);
+                    return new Goomba(position);
                 case "Koopa":
                     // Assuming Koopa implements IEnemy
-                    return new Koopa(entityPosition[type]);
+                    return new Koopa(position);
                 case "Block":
                     // Assuming Block implements IBlock
-                    return new Block(entityPosition[type]);
+                    return new Block(position);
                 case "Item":
                     // Assuming Item implements IItem
-                    return new Item(items, entityPosition[type]);
+                    return new Item(items, position);
                 // Add cases for other entities as needed
                 default:
                     throw new KeyNotFoundException($"Entity type {type} not recognized.");
