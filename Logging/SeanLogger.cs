1849ef40b4ba7e7719db0d572fd05b971980334a
Sean Kelley <sean.kelley@neudesic.com>
2024-02-27 22:17:01 -0500

    Init: Adds simple logger that logs to a file

---
+++ Logging/SeanLogger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Mario.Logging
+{
+    public class SeanLogger
+    {
+        private static SeanLogger instance = new SeanLogger();
+        private readonly string logFilePath; // Path to the log file
+        private StreamWriter logFileWriter; // StreamWriter for writing logs
+
+
+        public static SeanLogger Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private SeanLogger()
+        {
+            // Set the path to the log file (you can customize this)
+            logFilePath = "sean_log.txt";
+
+            // Initialize the StreamWriter to append to the log file
+            logFileWriter = new StreamWriter(logFilePath, append: true);
+
+            // Log that the log file was created
+            logFileWriter.WriteLine("Log file created at " + DateTime.Now);
+        }
+
+
+        // Log an error to the file
+        public void LogError(string message)
+        {
+            logFileWriter.WriteLine("ERROR: " + message);
+        }
+
+        // Log a warning to the file
+        public void LogWarning(string message)
+        {
+            logFileWriter.WriteLine("WARNING: " + message);
+        }
+
+        // Log an info message to the file
+        public void LogInfo(string message)
+        {
+            logFileWriter.WriteLine("INFO: " + message);
+        }
+
+        // Dispose method to close the StreamWriter
+        public void Dispose()
+        {
+            logFileWriter.Dispose();
+        }
+    }
+}
