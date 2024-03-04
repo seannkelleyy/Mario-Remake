e4aa6d9a3a9110756fbd238ccdb35169732cc54f
Jinyi <103870101+SharkKnightGeis@users.noreply.github.com>
2024-03-03 20:42:46 -0500

    Update CollisionDetector.cs
    now it will return a bool array

--- Collisions/CollisionDetector.cs
+++ Collisions/CollisionDetector.cs
@@ -1,29 +1,27 @@
 using Mario.Interfaces.Entities;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input.Touch;
 
 public class CollisionDetector
 {
-    public static CollisionDirection DetectCollision(Rectangle obj1, Rectangle obj2){
+    public static bool[] DetectCollision(Rectangle obj1, Rectangle obj2)
+    {
+        //default to be not collided
+        bool top = false;
+        bool bot = false;
+        bool left = false;
+        bool right = false;
+
         Rectangle intersection = Rectangle.Intersect(obj1, obj2);
         if (!intersection.IsEmpty)
         {
-            if(intersection.Width >= intersection.Height)
-            {
-                if(obj1.X > obj2.X)
-                {
-                    return Left;
-                }
-                return Right;
-            }
-            else
-            {
-                if (obj1.Y > obj2.Y)
-                {
-                    return Bottom;
-                }
-                return Top;
-            }
+            top = obj1.Bottom > obj2.Top; // Collision on the top side of obj2 
+            bot = obj1.Top < obj2.Bottom; // Collision on the bottom side of obj2
+            left = obj1.Right > obj2.Left; // Collision on the left side of obj2
+            right = obj1.Left < obj2.Right; // Collision on the right side of obj2
         }
-        return NoCollision;
+
+        bool[] collision = new bool[] { top, bot, left, right };
+        return collision;
     }
 }
