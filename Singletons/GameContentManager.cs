6cb91614e3bf5731d467acead58cad6d13c3acaf
Sean Kelley <sean.kelley@neudesic.com>
2024-03-03 20:59:03 -0500

    Fix: Removes duplicate methods in GCM

--- Singletons/GameContentManager.cs
+++ Singletons/GameContentManager.cs
@@ -1,6 +1,5 @@
 ﻿using Mario.Interfaces;
 using Mario.Interfaces.Entities;
-using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -41,7 +40,7 @@ namespace Mario.Singletons
 
         public IHero GetHero()
         {
-            return mario; 
+            return mario;
         }
 
         public void AddEntity(IEntityBase entity)
@@ -61,48 +60,6 @@ namespace Mario.Singletons
             else if (entity is IBlock)
             {
                 blocks.Append((IBlock)entity);
-            }
-        }
-
-        public void AddEntity(IEntityBase entity)
-        {
-            if (entity is IHero)
-            {
-                mario = (IHero)entity;
-            }
-            else if (entity is IEnemy)
-            {
-                enemies.Append((IEnemy)entity);
-            }
-            else if (entity is IItem)
-            {
-                items.Append((IItem)entity);
-            }
-            else if (entity is IBlock)
-            {
-                blocks.Append((IBlock)entity);
-            }
-        }
-
-        // Not sure how to procede here, since we don't have a way of 
-        // knowing which entity to remove
-        public void RemoveEntity(IEntityBase entity)
-        {
-            if (entity is IHero)
-            {
-                mario = null;
-            }
-            else if (entity is IEnemy)
-            {
-                // Remove enemy from list
-            }
-            else if (entity is IItem)
-            {
-                // Remove item from list
-            }
-            else if (entity is IBlock)
-            {
-                // Remove block from list
             }
         }
 
