937a9b2f2a35cc009fbf90e47f8102725466f4d8
FleePompeii <103870101+SharkKnightGeis@users.noreply.github.com>
2024-03-06 12:09:16 -0500

    Add small Mario states

---
+++ Entities/Hero/HeroStates/SmallStates/HeroJumpLeftState.cs
@@ -0,0 +1,9 @@
+﻿using Mario.Entities.Character.HeroStates;
+
+public class JumpLeftState : HeroState
+{
+    public JumpLeftState() : base()
+    {
+        sprite = spriteFactory.CreateSprite("leftJumpMario");
+    }
+}
