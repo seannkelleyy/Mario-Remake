07e62f73846060f1650b123263af520c62973d6c
hood276 <157760241+hood276@users.noreply.github.com>
2024-04-18 20:54:15 -0400

    IH-add-full-screen (#145)
    * Added full screen capability and fixed an issue with the bullet bill.
    
    * Got rid of magic numbers.
    
    * Adjusted background color.
    
    * Added ability to toggle full screen mode by pressing the 'f' key.

--- Game.cs
+++ Game.cs
@@ -21,12 +21,16 @@ namespace Mario
         public MarioRemake()
         {
             graphics = new GraphicsDeviceManager(this);
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
+            graphics.PreferredBackBufferHeight = GameSettings.InitialWindowHeight;
+            graphics.IsFullScreen = true;
+
             keyboardController = new KeyboardController();
             gamePadController = new GamePadController();
 
@@ -116,7 +120,7 @@ namespace Mario
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             spriteBatch.Begin(transformMatrix: camera.Transform);
             MediaManager.Instance.Draw(spriteBatch);
@@ -128,6 +132,13 @@ namespace Mario
             spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        public void ChangeFullScreenMode()
+        {
+            graphics.ToggleFullScreen();
+            graphics.PreferredBackBufferHeight = GameSettings.WindowHeight;
+            graphics.ApplyChanges();
         }
     }
 }
